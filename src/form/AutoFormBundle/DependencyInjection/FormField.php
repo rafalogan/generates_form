13d08158dd666a8e199249dbb5d455395a81cd3b
RafaDraw <www.rafadraw@gmail.com>
2016-09-23 14:23:24 -0300

    Icon & attr <input

--- src/form/AutoFormBundle/DependencyInjection/FormField.php
+++ src/form/AutoFormBundle/DependencyInjection/FormField.php
@@ -23,15 +23,45 @@ class FormField{
         if ($this->Type == "materialize"):
             $fields = $this->Fields;
             foreach ($fields as $key => $value ):
-                self::setInputField($value['div']);
+                $value['div']       = self::setInputField($value['div']);
+                $value['icon']      = self::setIcon($value['icon']);
+                $value['attr']      = self::setAttr($value['attr']);
+                $value['select']    = self::setAttr($value['select']);
             endforeach;
 
             $this->Fields = $fields;
         endif;
     }
 
+     /** Validação interna dos campos */
+
     static function setInputField($Group){
-        if(empty($Group['class'])) $Group['class'] = "input-field";
+        if(empty($Group['class']))  $Group['class'] = "input-field";
         if(!empty($Group['class'])) $Group['class'] = "input-field " . $Group['class'];
+        return $Group;
+    }
+
+    static function setIcon($I){
+        if (empty($I)) $I = "";
+
+        if (!empty($I)):
+            if(empty($I['class']))  $I['class'] = "";
+            if(empty($I['name']))   $I['name'] = "";
+        endif;
+
+        return $I;
+    }
+
+    static function setAttr($Attr){
+        if (empty($Attr)) $Attr = "";
+        if (!empty($Attr) && $Attr['type'] != "switch"):
+            if (empty($Attr['class']))  $Attr['class'] = "validate";
+            if (!empty($Attr['class'])) $Attr['class'] = "validate" . $Attr['class'];
+        endif;
+        return $Attr;
+    }
+
+    static function setSelect($Select){
+        return $Select;
     }
 }
